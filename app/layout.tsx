0165451393b2c9c9eadbc1fcf202f6e98b9db04d
shourav <shouravrahman006@gmail.com>
2024-12-03 06:04:00 +0600

    chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const font = Poppins(
   { subsets: ["latin"],
@@ -24,13 +25,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
         <body
 
       suppressHydrationWarning={true}
       className={font.className}>
         <Analytics />
         {children}
-
+           <script
+              dangerouslySetInnerHTML={{
+                 __html: `
+              (function(d, t) {
+                var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+                v.onload = function() {
+                  window.voiceflow.chat.load({
+                    verify: { projectID: '674e424d49b8573a0d523b73' },
+                    url: 'https://general-runtime.voiceflow.com',
+                    versionID: 'production'
+                  });
+                }
+                v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+                v.type = "text/javascript";
+                s.parentNode.insertBefore(v, s);
+              })(document, 'script');
+            `
+              }}
+           />
         </body>
     </html>
   );
